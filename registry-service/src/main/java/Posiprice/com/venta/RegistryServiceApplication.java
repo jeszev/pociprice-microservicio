675ccb2be1fbd94971a0f87ef53413f4540f33ef
Jesus Zevallos <and.zevja@gmail.com>
2020-10-02 01:21:47 -0500

    Posiprice-microservicio master

---
+++ registry-service/src/main/java/Posiprice/com/venta/RegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package Posiprice.com.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}
